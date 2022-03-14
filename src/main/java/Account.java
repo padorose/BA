5b322b94900450aae0bd8768c0f839ebf0f89775
uzytkownik <uzytkownik@kursGIT.pl>
2022-03-14 19:53:55 +0100

    add cosmetic things

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Account {
     private int accountNo;
     private String accountName;
     private String accountSurname;
     private long balance;
+
+    Scanner scanner=new Scanner(System.in);
 
     public Account(int accountNo, String accountName, String accountSurname, long balance){
         this.accountName=accountName;
@@ -18,6 +22,21 @@ public class Account {
     }
     public void setAccountNo(int accountNo){
         this.accountNo=accountNo;
+    }
+    public void openAccount(){
+        System.out.println("Enter No");
+        accountNo=scanner.nextInt();
+        System.out.println("Enter Name");
+        accountName=scanner.next();
+        System.out.println("Enter Surname");
+        accountSurname=scanner.next();
+        System.out.println("Enter Balance");
+        balance=scanner.nextInt();
+
+    }
+    public void showAccount(){
+        System.out.println("Account number is: " + accountNo +". Name is: "+ accountName+ ". " +
+                "Surname is: "+accountSurname + ". Your balance is: "+ balance + "." );
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args){
-       Account user1 = new Account(101,"Jan","Kowalski",10000);
        Account user2 = new Account();
+    user2.openAccount();
+user2.showAccount();
 
-
-        System.out.println(user1.getAccountNo());
-        user2.setAccountNo(102);
-        System.out.println(user2.getAccountNo());
     }
 }
